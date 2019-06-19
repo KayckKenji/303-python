3e0c35c7fc99209259a9c25bf5bbe4516a45a04a
Kenji <51339721+KayckKenji@users.noreply.github.com>
2019-06-19 17:04:55 -0300

    início a orientação a objetos

---
+++ cebola.py
@@ -0,0 +1,59 @@
+
+
+#lista:********************************************
+
+#alho = ["arroz", "feijao", "pao", "pate"]
+    #alho.pop ()
+    #print("o segundo elemento é {}"format (alho[2]))
+    #print("o terceiro elemento é {}"format (alho[3]))
+    #del (alho[2])
+    #print("o segundo elemento é {}"format (alho[2]))
+    #print("o terceiro elemento é {}"format (alho[3]))
+
+
+
+
+#def minha_funcao("OI, ", Eric")
+#if a = True
+#minha_funcao("OI, ", Eric")
+#a = True
+
+#usuario = kenji
+#senha = 1234
+
+#usuario = input ("Digite o nome de usuário")
+    #print(usuario)
+
+#senha = input ("Digite a senha")
+    #print(senha)
+
+usuario_corr = "kenji"
+senha_corr = "1234"
+contador = 0
+
+#while contador != 3:
+ #   usuario = input ("Digite o nome de usuário: ")
+  #  senha = input ("Digite a senha: ")
+   # if usuario == usuario_corr and int (senha) == senha_corr:
+    #    print ("Usuário autenticado")
+    #else:
+    #]
+    # 2print ("Dados incorretos")
+#contador += 1
+ #  print ("Você excedeu as tentativas")
+
+class Animal()
+
+
+def __inuit__(self, nome, perna, especie)
+
+
+gato =  Animal("Bichano", 4, "Felino")
+# gato = 4
+print(gato.nome)
+
+gato2 =  Animal("Tom", 3, "Felino")
+print(gato2.andar)
+
+
+
